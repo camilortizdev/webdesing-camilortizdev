fa92260e3efced7536942eb7683f0fceb2d4536d
kmilo <camiloweb9@gmail.com>
2023-02-13 12:49:21 -0500

    Primer commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+     <header className="flex items-center w-full p-4">
+        <div className="bg-red-300 w-1/6">
+            <h1>Logo</h1>
+        </div>
+        <nav className="bg-red-600 flex-1">
+            <h1>Nav</h1>
+        </nav>
+    </header>
+    )
+};
+
+export default Header;
